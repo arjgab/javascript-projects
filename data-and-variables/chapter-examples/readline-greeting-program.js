05b21bd70429e0bb6d9b8c6ddaa93f2f63551427
aleirg <aargbl5@gmail.com>
2024-05-25 16:11:27 -0500

    updated chapter examples

--- data-and-variables/chapter-examples/readline-greeting-program.js
+++ data-and-variables/chapter-examples/readline-greeting-program.js
@@ -1,3 +1,9 @@
 const input = require('readline-sync');
 
 let name = input.question("Enter your name: ");
+let otherName = input.question("Enter other name: ");
+
+console.log("Hello, " + name + "!");
+console.log("First name: " + name);
+console.log("Last Name: " + otherName);
+console.log("Last, First: " + otherName + ", " + name);
